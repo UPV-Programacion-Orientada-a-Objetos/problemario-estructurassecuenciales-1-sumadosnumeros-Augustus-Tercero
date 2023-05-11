d7cc7d795b21c7819ccf51d22aca7d7bb4724c9d
Agustín Alejandro Mota Hinojosa <2030406@upv.edu.mx>
2023-05-11 11:27:20 -0600

    finish

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
-
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {
+    SumaDosNumeros obj = new SumaDosNumeros();
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println(obj.suma(a, b));
+  }
 }
